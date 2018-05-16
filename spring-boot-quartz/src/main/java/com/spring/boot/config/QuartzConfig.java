85282e2d1c437e1ce3f70437ffbab4b2fed79511
admin <zcjswing@163.com>
2018-05-16 11:13:38 +0800

    no message

--- spring-boot-quartz/src/main/java/com/spring/boot/config/QuartzConfig.java
+++ spring-boot-quartz/src/main/java/com/spring/boot/config/QuartzConfig.java
@@ -13,7 +13,11 @@ import org.springframework.context.annotation.Configuration;
 
 import com.spring.boot.job.OneJob;
 import com.spring.boot.job.SecondJob;
-
+/**
+ * Quartz配置文件.<p/>
+ * 
+ * 创建时间：2018年5月16日
+ */
 @Configuration
 public class QuartzConfig {
 
@@ -22,26 +26,35 @@ public class QuartzConfig {
 		return TriggerBuilder.newTrigger();
 	}
     
-    /*
-     * 通过SchedulerFactoryBean获取Scheduler的实例
+    /**
+     * 开始调度任务
+     * 
+     * <li>job具体执行任务</li>
+     * <li>trigger触发器</li>
+     * <li>scheduler调度器</li>
+     * @return
+     * @throws Exception
      */
     @Bean(name="Scheduler")
     public Scheduler scheduler() throws Exception {
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
         
+        // 获取具体要执行的job
         JobDetail oneJob = JobBuilder.newJob(OneJob.class).withIdentity("job1", "group1").build();
         JobDetail secondJob = JobBuilder.newJob(SecondJob.class).withIdentity("job2", "group2").build();
         
+        // 定义具体触发器
         TriggerBuilder<Trigger> newTrigger = trigger();
-        
 		Trigger oneTrigger = newTrigger.withIdentity("trigger1", "group1")
 				.withSchedule(CronScheduleBuilder.cronSchedule("0/2 * * * * ?")).build();
 		Trigger secondTrigger = newTrigger.withIdentity("trigger2", "group2")
 				.withSchedule(CronScheduleBuilder.cronSchedule("0/4 * * * * ?")).build();
 		
+		// Scheduler将具体的job匹配对应的触发器Trigger
 		scheduler.scheduleJob(oneJob, oneTrigger);
 		scheduler.scheduleJob(secondJob, secondTrigger);
 		
+		// 开始调度任务
 		scheduler.start();
 		
 		return scheduler;
